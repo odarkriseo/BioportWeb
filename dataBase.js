7fc8cee1411f87222ce28c16e574b1bdcf80cccc
odarkriseo <gauvin.alexandre.35@gmail.com>
2015-09-07 10:19:06 +0200

    database ok

--- dataBase.js
+++ dataBase.js
@@ -8,7 +8,6 @@ var app = express();
 
 app.use(function(req, res, next) {  
   res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header(
     'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key'
